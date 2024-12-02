4db898b8755bf9e5dd00e72c953cb319b525a04a
Oscar Börjesson <oscar.boerjesson@gmail.com>
2024-12-02 13:06:56 +0100

    Changed d2p1, completed d2p2

--- d2/d2p1.py
+++ d2/d2p1.py
@@ -1,18 +1,11 @@
 with open('d2/input.txt') as file:
     lines = file.readlines()
 
-safe = 0
-for line in lines:
-    line = line.strip()
-    line = line.split()
-    lineNums = []
-    for num in line:
-        lineNums.append(int(num))
-    
+def is_safe(report):
     diffs = []
-    prevNum = lineNums[0]
-    for i in range(1,len(lineNums)):
-        currentNum = lineNums[i]
+    prevNum = report[0]
+    for i in range(1,len(report)):
+        currentNum = report[i]
         diff = currentNum - prevNum
         diffs.append(str(diff))
         prevNum = currentNum
@@ -21,22 +14,34 @@ for line in lines:
     negList = ["-1","-2","-3"]
 
     if "0" in diffs:
-        continue
+        return False
+    
     check = True
     for e in diffs:
         if e not in posList:
             check = False
     if check:
-        safe += 1
-        continue
+        return True
+    
     check = True
     for e in diffs:
         if e not in negList:
             check = False
     if check:
-        safe += 1
-        continue
+        return True
+    
     else:
-        continue
+        return False
+
+safe = 0
+for line in lines:
+    line = line.strip()
+    line = line.split()
+    lineNums = []
+    for num in line:
+        lineNums.append(int(num))
+    
+    if is_safe(lineNums):
+        safe += 1
 print(safe)
         
--- d2/d2p2.py
+++ d2/d2p2.py
@@ -1,3 +1,54 @@
 with open('d2/input.txt') as file:
     lines = file.readlines()
 
+def is_safe(report):
+    diffs = []
+    prevNum = report[0]
+    for i in range(1,len(report)):
+        currentNum = report[i]
+        diff = currentNum - prevNum
+        diffs.append(str(diff))
+        prevNum = currentNum
+    
+    posList = ["1","2","3"]
+    negList = ["-1","-2","-3"]
+
+    if "0" in diffs:
+        return False
+    
+    check = True
+    for e in diffs:
+        if e not in posList:
+            check = False
+    if check:
+        return True
+    
+    check = True
+    for e in diffs:
+        if e not in negList:
+            check = False
+    if check:
+        return True
+    
+    else:
+        return False
+    
+def is_safe_damp(report):
+    if is_safe(report):
+        return True
+    for i in range(len(report)):
+        if is_safe(report[:i]+report[i+1:]):
+            return True
+    return False
+
+safe = 0
+for line in lines:
+    line = line.strip()
+    line = line.split()
+    lineNums = []
+    for num in line:
+        lineNums.append(int(num))
+    
+    if is_safe_damp(lineNums):
+        safe += 1
+print(safe)
