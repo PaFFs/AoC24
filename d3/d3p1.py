e3eaa5a3d801c3895eee8ba76c9a3dc41fe82b0e
Oscar Börjesson <oscar.boerjesson@gmail.com>
2024-12-03 10:26:51 +0100

    d3p1 complete, d3p2 started

--- d3/d3p1.py
+++ d3/d3p1.py
@@ -1,2 +1,18 @@
-with open('d/input.txt') as file:
+with open('d3/input.txt') as file:
     lines = file.readlines()
+
+import re
+
+compLine = ""
+for line in lines:
+    line.strip()
+    compLine += line
+
+muls = re.findall(r'mul\([0-9]+\,[0-9]+\)',compLine)
+
+sum = 0
+for mul in muls:
+    mul = mul[4:-1]
+    mul = mul.split(",")
+    sum += int(mul[0]) * int(mul[1])
+print(sum)
--- d3/d3p2.py
+++ d3/d3p2.py
@@ -1,2 +1,26 @@
-with open('d/input.txt') as file:
+with open('d3/input.txt') as file:
     lines = file.readlines()
+
+import re
+
+compLine = ""
+for line in lines:
+    line.strip()
+    compLine += line
+
+def mulLine(line):
+    muls = re.findall(r'mul\([0-9]+\,[0-9]+\)',line)
+
+    sum = 0
+    for mul in muls:
+        mul = mul[4:-1]
+        mul = mul.split(",")
+        sum += int(mul[0]) * int(mul[1])
+    return sum
+
+def splitLine(line):
+    splitLines = []
+    
+    return splitLines
+
+print(mulLine(compLine))
